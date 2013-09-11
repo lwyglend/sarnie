2350d0f08cfd367707450d91b892fc8482ad5712
lwyglend <lwyglend@thoughtworks.com>
2013-09-11 16:08:48 +0100

    Added Pod folder and Podfile.lock to gitignore

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '5.0'
+
+target :sarnie do
+    pod 'Reveal-iOS-SDK'
+end
+
+target :sarnieTests do
+    pod 'OCMock'
+    pod 'OCHamcrest'
+end
---
+++ sarnieTests/algorithmTests.h
@@ -0,0 +1,13 @@
+//
+//  algorithmTests.h
+//  algorithmTests
+//
+//  Created by Laura Wyglendacz on 09/08/2013.
+//  Copyright (c) 2013 Laura Wyglendacz. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface algorithmTests : SenTestCase
+
+@end
